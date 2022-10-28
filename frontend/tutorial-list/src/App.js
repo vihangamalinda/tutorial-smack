0d3f7b72a61faf4a162ede0eb78d61e6adbb67cf
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-10-29 01:00:03 +0530

    Folder structure.

---
+++ frontend/tutorial-list/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router,Switch, Link , Route } from "react-router-dom";
+
+import AddTutorial from "./components/AddTutorial.jsx"
+import Tutorial from "./components/Tutorial"
+import TutorialList from "./components/TutorialList.jsx"
+
+
+function App() {
+  return (
+    
+      <Router>
+        
+
+      </Router>
+  );
+}
+
+export default App;
