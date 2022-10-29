f7f30c2ecd618cfa5aef27ce3db943ed93558120
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2022-10-29 15:12:34 +0530

    Routing.

--- frontend/tutorial-list/src/App.js
+++ frontend/tutorial-list/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router,Switch, Link , Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes,  Link , Route } from "react-router-dom";
 
 import AddTutorial from "./components/AddTutorial.jsx"
 import Tutorial from "./components/Tutorial"
@@ -12,8 +12,26 @@ function App() {
   return (
     
       <Router>
-        
-
+        <nav className='navbar navbar-expand '>
+          <a href='/tutorials' className='navbar-brand'>Vihanga_M</a>
+          <div className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link to={"/tutorials"} className="nav-link"> Tutorial
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to={"/add"} className="nav-link"> Add Tutorial
+              </Link>
+            </li>
+          </div>
+        </nav>
+        <div className='container mt-3'>
+          <Routes>
+            <Route exact path={["/","tutorials"]} component={TutorialList}/>
+            <Route exact path='/add' component={AddTutorial}/>
+            <Route exact path='/tutorials/:id' component={Tutorial}/>
+          </Routes>
+        </div>
       </Router>
   );
 }
